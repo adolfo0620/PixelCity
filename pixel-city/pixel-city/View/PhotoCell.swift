82ff8eb8309475b29646071ef9f84c0e760d82ea
Adolfo Reyes <adolfo0620@gmail.com>
2017-11-28 20:34:21 -0500

    new collectionView cell and updating git ignore

---
+++ pixel-city/pixel-city/View/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  pixel-city
+//
+//  Created by adolfo reyes on 11/28/17.
+//  Copyright © 2017 adolfo reyes. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
